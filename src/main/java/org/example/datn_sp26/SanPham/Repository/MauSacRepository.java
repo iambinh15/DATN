66f12ddcb2f9f017e26afac85543a96969076f3e
tungbtph36245 <150203883+bttungg@users.noreply.github.com>
2026-02-11 23:12:27 +0700

    Delete src/main/java/org/example/datn_sp26/SanPham directory

--- src/main/java/org/example/datn_sp26/SanPham/Repository/MauSacRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.datn_sp26.SanPham.Repository;
-
-import org.example.datn_sp26.SanPham.Entity.MauSac;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MauSacRepository extends JpaRepository<MauSac, Integer> {
-    List<MauSac> findByTrangThai(Integer trangThai);
-}
--- src/main/java/org/example/datn_sp26/SanPham/Repository/ThuongHieuRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.datn_sp26.SanPham.Repository;
-
-import org.example.datn_sp26.SanPham.Entity.ThuongHieu;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ThuongHieuRepository extends JpaRepository<ThuongHieu, Integer> {
-    List<ThuongHieu> findByTrangThai(Integer trangThai);
-}
