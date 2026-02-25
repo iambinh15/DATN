c8c247a40a4130bf0692889f06a57571865303d5
iambinh15 <iambinh.contact@gmail.com>
2026-02-25 14:11:42 +0700

    sua hien thi san pham hoat dong va hien thi day du thuoc tinh spct

--- src/main/java/org/example/datn_sp26/SanPham/Controller/SanPhamController.java
+++ src/main/java/org/example/datn_sp26/SanPham/Controller/SanPhamController.java
@@ -24,13 +24,13 @@ public class SanPhamController {
     @Autowired
     private SanPhamChiTietRepository spctRepo;
 
-    // 1. TRANG CHỦ: Hiển thị danh sách sản phẩm chung
+    // 1. TRANG CHỦ: Chỉ hiển thị danh sách sản phẩm đang hoạt động (trangThai = 1)
     @GetMapping("/trang-chu")
     public String trangChu(Model model) {
-        model.addAttribute("listSanPham", sanPhamRepo.findAll());
+        //1 là trạng thái sản phẩm đang hoat đông nếu đổi là 0 thì sex là sản phẩm không hoạt động
+        model.addAttribute("listSanPham", sanPhamRepo.findByTrangThai(1));
         return "khachhang/trang-chu";
     }
-
     // 2. TRANG CHI TIẾT: Hiển thị 1 sản phẩm và các biến thể của nó
     @GetMapping("/san-pham/{id}")
     public String chiTiet(@PathVariable("id") Integer id, Model model) {
