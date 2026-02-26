eb0ce94a5b883c29b79faca498512389d0fb5289
tungbtph36245 <150203883+bttungg@users.noreply.github.com>
2026-02-26 20:05:29 +0700

    Delete DATN_SP26 directory

--- DATN_SP26/src/main/java/org/example/datn_sp26/SanPham/Repository/ChatLieuRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.datn_sp26.SanPham.Repository;
-
-import org.example.datn_sp26.SanPham.Entity.ChatLieu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatLieuRepository extends JpaRepository<ChatLieu, Integer> {
-    ChatLieu findByTenChatLieu(String tenChatLieu);
-}
